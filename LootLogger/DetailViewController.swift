aaf39d9ce712d290147e64f7dc571c991ab2d2a9
bayley <bayley019@outlook.com>
2025-11-19 23:11:40 -0500

    Alert controllers

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -12,6 +12,18 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in print("Present camera")}
+        alertController.addAction(cameraAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in print("Present photo library")}
+        alertController.addAction(photoLibraryAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     
